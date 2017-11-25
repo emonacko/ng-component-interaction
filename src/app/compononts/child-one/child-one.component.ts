0947ff14043cd13e712ff9075604c29f54ad6e12
Imran Hassan <imran@ackosoft.com>
2017-11-25 18:16:51 +0600

    rename output method

--- src/app/compononts/child-one/child-one.component.ts
+++ src/app/compononts/child-one/child-one.component.ts
@@ -23,7 +23,7 @@ export class ChildOneComponent implements OnInit {
   @Input('studentAddMsg')
   addMsg: string;
 
-  @Output('addStudentEvent')
+  @Output()
   addStdEvent = new EventEmitter<Student>();
 
   @Output()
